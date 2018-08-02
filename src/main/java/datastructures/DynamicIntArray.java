9de58de8320ab344796f2b5935f0a6523183c1da
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-08-02 22:18:40 +0200

    Create new class DynamicIntArray
    Create new class with it's fields and constructor

---
+++ src/main/java/datastructures/DynamicIntArray.java
@@ -0,0 +1,14 @@
+package datastructures;
+
+public class DynamicIntArray {
+
+    private int[] array;
+    private int lastIndex = 0;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public DynamicIntArray(int i) {
+        this.array = new int[i];
+    }
+
+}
