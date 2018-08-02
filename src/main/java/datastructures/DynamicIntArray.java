f3ba196ca81e3bce3a9603a66682ce3c63fdfc4a
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-08-02 22:21:38 +0200

    Add add/remove methods in DynamicIntArray class
    Add private helper methods to encapsulate code

--- src/main/java/datastructures/DynamicIntArray.java
+++ src/main/java/datastructures/DynamicIntArray.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import java.util.Arrays;
+
 public class DynamicIntArray {
 
     private int[] array;
@@ -13,6 +15,41 @@ public class DynamicIntArray {
 
     public DynamicIntArray(int i) {
         this.array = new int[i];
+    }
+
+    public void add(int element) {
+        changeCapacity();
+        array[lastIndex++] = element;
+
+    }
+
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
+        validateIndex(index);
+        if (index == lastIndex) {
+            lastIndex--;
+        } else if (index < lastIndex) {
+            removeFrom(index);
+            lastIndex--;
+        }
+    }
+
+    private void changeCapacity() {
+        if (lastIndex == (array.length-1)) {
+            int newSize = array.length * 2;
+            array = Arrays.copyOf(array, newSize);
+        }
+    }
+
+    private void validateIndex(int i) throws ArrayIndexOutOfBoundsException {
+        if (i > (lastIndex - 1) || i < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    private void removeFrom(int index) {
+        for (int i = index; i < lastIndex; i++) {
+            array[i] = array[i+1];
+        }
     }
 
 }
